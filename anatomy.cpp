0c8da1cf06960b0474da268cd7c2d1f615521a21
WillDevUnreal <willdevunreal@gmail.com>
2022-06-08 19:11:26 -0300

    studies code

---
+++ anatomy.cpp
@@ -0,0 +1,28 @@
+//Every Line  start with # is a preprocessor directivies
+/*
+    This is a directive what tells the preprocessor to
+    include the contents of file
+*/
+#include <iostream>
+
+int main(){
+    // how is a book is organized?
+
+    // A code block is a grouping of one or more statements.
+    {
+        ; // a statement is a syntactic unit of an imperative programing language
+        ;
+    }
+
+    std::cout
+        <<
+        "Hi there!"
+        <<
+        "Bye!"
+        <<
+        std::endl;
+
+
+    std::cout << "end!";
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+//pre processor directives
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World";
+    return 0;
+}
