d8cb0521a10151f7936af0b71502d500066fd33b
WillDevUnreal <willdevunreal@gmail.com>
2022-06-12 18:00:20 -0300

    studies dataTypes

--- string.cpp
+++ string.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main(){
+    string title = "Mr. ";
+    string name("Will");
+
+    cout << name.size() <<endl;
+    cout << name.back() <<endl;
+    cout << name[0] <<endl;
+    cout << title + name <<endl;
     return 0;
 }
